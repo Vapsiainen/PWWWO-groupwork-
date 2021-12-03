1bcea6c18c7b649ee5c6b5395565bf94a1a410d9
Vapsiainen <59833410+Vapsiainen@users.noreply.github.com>
2021-12-03 04:21:35 +0200

    Error handling ja muita muutoksia

--- App/main.js
+++ App/main.js
@@ -1,10 +1,14 @@
+"use strict";
+
 const express = require("express"),
-  app = express(),
-  http = require("http");
-  const httpStatus = require("http-status-codes");
+  layouts = require("express-ejs-layouts"),
+  http = require("http"),
+  httpStatus = require("http-status-codes"),
   contactController = require("./controllers/contactController"),
   indexController = require("./controllers/indexController"),
-  layouts = require("express-ejs-layouts");
+  errorController = require("./controllers/errorController");
+
+const app = express();
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
@@ -18,26 +22,26 @@ app.use(
 );
 app.use(express.json());
 
+
+// R O U T E T
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome</h1>");
+})
+
+app.get("/contact", contactController.getContactPage);
+app.get("/index", indexController.getIndexPage);
+
 app.post("/", (req, res) => {
   console.log(req.body);
   console.log(req.query);
   res.send("POST Successful!");
 });
 
-app.on("request", (req, res) => {
-  console.log("Received an incoming request.");
-  res.writeHead(httpStatus.StatusCodes.OK, {
-      "Content-Type": "text/html"
-  });
 
-  let responseMessage = "<h1>Welcome!</h1>";
-  res.write(responseMessage);
-  res.end();
-  console.log(`Sent a response: ${responseMessage}`);
-});
+app.use(errorController.logErrors);
+app.use(errorController.respond404);
+app.use(errorController.respond500);
 
-app.get("/contact", contactController.getContactPage);
-app.get("/index", indexController.getIndexPage)
 
 app.listen(app.get("port"), () => {
   console.log(`Server running at http://localhost:${app.get("port")}`);
